e1174c3a4e8afef51bf8be471c5ae2a9605e1258
balmoorthy <tamilanbala72@gmail.com>
2024-11-16 21:15:20 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZmQ1MmYxODVkZDI4YTdjMThlMDdhY2Q4NjU4ZWYzZSIsIm5iZiI6MTczMTc2OTkzNy4xNDQ4Nzk2LCJzdWIiOiI2NzM4YjJjNzk2YmUzYTA2ZmFkOTI0NGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.uuakQrIsTaJkwTTxL1-RgNmAC4z34R-bNFa2l39yg5k",
+    },
+  };
+
+  fetch(
+    "https://api.themoviedb.org/3/account/21634162/rated/movies?language=en-US&page=1&sort_by=created_at.asc",
+    options
+  )
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
