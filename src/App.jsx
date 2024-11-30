11e92e7223ea3a716b53e64bc2d75515229b0a6c
balmoorthy <tamilanbala72@gmail.com>
2024-11-30 12:15:01 +0400

    update apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,29 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BASE_URL } from "./utils/constants";
+
+const API_KEY = import.meta.env.API_KEY || "1fd52f185dd28a7c18e07acd8658ef3e"; // Replace with your TMDB API Key
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "",
-    },
+  const fetchPopularMovies = async () => {
+    try {
+      const response = await fetch(
+        `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+      console.log("Popular Movies:", data.results); // Movie list
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
   };
 
-  fetch(
-    "https://api.themoviedb.org/3/account/21634162/rated/movies?language=en-US&page=1&sort_by=created_at.asc",
-    options
-  )
-    .then((res) => res.json())
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+  // Call the function
+  fetchPopularMovies();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const PAGE_SIZE = 10;
+export const BASE_URL = "https://api.themoviedb.org/3";
