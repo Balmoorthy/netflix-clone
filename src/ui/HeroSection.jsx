585f6d3b518867d508c0b856f5309c5d36b638d6
balmoorthy <tamilanbala72@gmail.com>
2024-12-27 22:17:15 +0400

    update hero section slides

--- src/ui/HeroSection.jsx
+++ src/ui/HeroSection.jsx
@@ -1,4 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+import { IoIosPause } from "react-icons/io";
+import { IoPlay } from "react-icons/io5";
 import styled from "styled-components";
 
 const images = [
@@ -8,42 +11,86 @@ const images = [
   "home-slider/home-slide-img-4.jpg",
   "home-slider/home-slide-img-5.jpg",
 ];
-const CarouselWrapper = styled.div`
+
+const CarouselWrapper = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "currentIndex",
+})`
   position: relative;
+  /* background: rgba(249, 146, 151, 0.2); */
   width: 100%;
-  margin: auto;
-  overflow: hidden;
+  max-width: 120rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 1rem;
+  margin-bottom: 3.6rem;
+  background: ${({ currentIndex }) =>
+    currentIndex === 0
+      ? "lightblue"
+      : currentIndex === 1
+      ? "lightgreen"
+      : "lightcoral"};
 `;
 
-const ImageContainer = styled.div`
+const StyledImgContainer = styled.div`
+  mask-image: radial-gradient(
+    103% 10.5% at 50% 102%,
+    transparent 50%,
+    white 52%
+  );
+  border-radius: 20px 20px 16px 16px;
+  filter: drop-shadow(24px 32px 24px rgba(0, 0, 0, 0.6));
+
+  &::before {
+    position: absolute;
+    content: "";
+    inset: 0;
+    padding: 2px;
+    z-index: 1;
+    background: radial-gradient(at top left, white 25%, transparent 70%);
+    mask-composite: exclude;
+    pointer-events: none;
+    mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+    z-index: 100;
+  }
+`;
+
+const ImageContainer = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "currentIndex",
+})`
+  display: flex;
+  transform: ${({ currentIndex }) => `translateX(-${currentIndex * 100}%)`};
+  transition: transform 0.5s ease-in-out;
+  inset: 1;
+  z-index: 2;
+  height: 100%;
+  /* background: radial-gradient(at top left, white 25%, transparent 70%); */
+
   img {
     width: 100%;
-    display: none;
-    transition: opacity 0.5s ease-in-out;
-
-    &.active {
-      display: block;
-      opacity: 1;
-    }
+    z-index: 300;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    filter: drop-shadow(24px 32px 24px rgba(0, 0, 0, 0.6));
   }
 `;
 
 const Controls = styled.div`
-  position: absolute;
+  /* position: absolute; */
   bottom: 10px;
   left: 0;
   right: 0;
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
   gap: 10px;
 `;
 
 const ProgressBarContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 80%;
+  align-items: center;
+  width: 40%;
   gap: 5px;
 `;
 
@@ -54,6 +101,7 @@ const ProgressBar = styled.div`
   position: relative;
   cursor: pointer;
   border-radius: var(--border-radius-md);
+  transition: height 0.5s linear;
 
   .progress {
     height: 100%;
@@ -64,20 +112,18 @@ const ProgressBar = styled.div`
 
   &:hover {
     height: 7px;
-    width: 7px;
   }
 `;
 
 const PlayPauseButton = styled.button`
-  background: #007bff;
-  color: white;
   border: none;
-  padding: 5px 10px;
+  padding: 0.25rem;
   cursor: pointer;
-  border-radius: 3px;
+  background: none;
+  outline-offset: none;
 
-  &:hover {
-    background: #0056b3;
+  &:active {
+    outline-offset: none;
   }
 `;
 
@@ -85,15 +131,23 @@ const NavigationButton = styled.button`
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
-  background: rgba(0, 0, 0, 0.5);
-  color: white;
+  background: var(--color-grey-600);
+  color: var(--color-grey-50);
   border: none;
-  padding: 10px;
+  width: 2.4rem;
+  height: 8rem;
+  border-radius: 16rem;
   cursor: pointer;
-  border-radius: 50%;
+  display: flex;
+  align-items: center;
+
+  & svg {
+    width: 24px;
+    height: 24px;
+  }
 
   &:hover {
-    background: rgba(0, 0, 0, 0.8);
+    background: var(--color-grey-800);
   }
 
   &.left {
@@ -116,7 +170,32 @@ const Carousel = () => {
 
   const DURATION = 5000; // 5 seconds for each slide
 
+  const changeSlide = useCallback(
+    (direction) => {
+      setProgressBars((prev) => {
+        const updated = [...prev];
+        updated[currentIndex] = 0;
+        return updated;
+      });
+
+      setCurrentIndex((prevIndex) => {
+        const nextIndex =
+          (prevIndex + direction + images.length) % images.length;
+        return nextIndex;
+      });
+
+      if (isPlaying) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = setInterval(() => {
+          changeSlide(1);
+        }, DURATION);
+      }
+    },
+    [currentIndex, isPlaying]
+  );
+
   useEffect(() => {
+    const localProgressRefs = progressRefs.current;
     if (isPlaying) {
       startProgress(currentIndex);
       intervalRef.current = setInterval(() => {
@@ -129,9 +208,9 @@ const Carousel = () => {
 
     return () => {
       clearInterval(intervalRef.current);
-      progressRefs.current.forEach(clearInterval);
+      localProgressRefs.forEach(clearInterval);
     };
-  }, [isPlaying, currentIndex]);
+  }, [isPlaying, currentIndex, changeSlide]);
 
   const startProgress = (index) => {
     clearInterval(progressRefs.current[index]);
@@ -149,26 +228,6 @@ const Carousel = () => {
     setIsPlaying((prev) => !prev);
   };
 
-  const changeSlide = (direction) => {
-    setProgressBars((prev) => {
-      const updated = [...prev];
-      updated[currentIndex] = 0;
-      return updated;
-    });
-
-    setCurrentIndex((prevIndex) => {
-      const nextIndex = (prevIndex + direction + images.length) % images.length;
-      return nextIndex;
-    });
-
-    if (isPlaying) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = setInterval(() => {
-        changeSlide(1);
-      }, DURATION);
-    }
-  };
-
   const handleProgressBarClick = (index) => {
     setProgressBars((prev) => {
       const updated = [...prev];
@@ -176,6 +235,7 @@ const Carousel = () => {
       return updated;
     });
     setCurrentIndex(index);
+    setIsPlaying(false);
 
     if (isPlaying) {
       clearInterval(intervalRef.current);
@@ -186,24 +246,30 @@ const Carousel = () => {
   };
 
   return (
-    <CarouselWrapper>
-      <ImageContainer>
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Slide ${index + 1}`}
-            className={index === currentIndex ? "active" : ""}
-          />
-        ))}
-      </ImageContainer>
+    <CarouselWrapper currentIndex={currentIndex}>
+      <StyledImgContainer currentIndex={currentIndex}>
+        <ImageContainer currentIndex={currentIndex}>
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className={index === currentIndex ? "active" : ""}
+            />
+          ))}
+        </ImageContainer>
+      </StyledImgContainer>
+
       <NavigationButton className="left" onClick={() => changeSlide(-1)}>
-        &#8249;
+        <GoChevronLeft />
       </NavigationButton>
       <NavigationButton className="right" onClick={() => changeSlide(1)}>
-        &#8250;
+        <GoChevronRight />
       </NavigationButton>
       <Controls>
+        <PlayPauseButton onClick={handlePlayPause}>
+          {isPlaying ? <IoIosPause /> : <IoPlay />}
+        </PlayPauseButton>
         <ProgressBarContainer>
           {images.map((_, index) => (
             <ProgressBar
@@ -217,9 +283,6 @@ const Carousel = () => {
             </ProgressBar>
           ))}
         </ProgressBarContainer>
-        <PlayPauseButton onClick={handlePlayPause}>
-          {isPlaying ? "Pause" : "Play"}
-        </PlayPauseButton>
       </Controls>
     </CarouselWrapper>
   );
